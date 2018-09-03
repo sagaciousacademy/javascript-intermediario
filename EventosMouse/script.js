049757b377999d55bda10b2cd3f8aea29a5de1dc
Marcelo <ajuda@sagaciousacademy.com.br>
2018-09-03 08:58:12 -0300

    Eventos do Mouse

---
+++ EventosMouse/script.js
@@ -0,0 +1,11 @@
+function clicou() {
+    console.log("CLICOU");
+}
+function botaoDireito() {
+    console.log("BOTAO DIREITO");
+
+    return false;
+}
+function cliqueDuplo() {
+    console.log("CLIQUE DUPLO");
+}
---
+++ EventosMouse/script2.js
@@ -0,0 +1,15 @@
+
+    console.log("APERTOU O MOUSE");
+}
+function soltouMouse() {
+    console.log("SOLTOU O MOUSE!");
+}
+function mouseEmCima() {
+    console.log("MOUSE POR CIMA");
+}
+function mouseSaiudeCima() {
+    console.log("MOUSE SAIU DE CIMA");
+}
+function mouseMoveu() {
+    console.log("MOUSE MOVEU POR CIMA");
+}
